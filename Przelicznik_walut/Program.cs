131ff326282757e64ddefef01df32e3a1b013d07
kamilcv <kamilcvv@gmail.com>
2021-03-19 21:51:45 +0100

    Pusty projekt
    Pusty projekt w C# do projektu

---
+++ Przelicznik_walut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Przelicznik_walut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
